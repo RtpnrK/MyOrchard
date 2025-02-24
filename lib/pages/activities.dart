153dbf64523b670afdb2c97f17361cbc618c0d19
rtpnrk <narata@kkumail.com>
2025-02-24 17:31:17 +0700

    combine

--- lib/pages/activities.dart
+++ lib/pages/activities.dart
@@ -1,81 +1,140 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:myorchard/models/activities_model.dart';
 import 'package:myorchard/pages/activityDetail.dart';
-import 'package:myorchard/pages/create_activity.dart';
+import 'package:myorchard/providers/activity_provider.dart';
+import 'package:provider/provider.dart';
 
 class Activities extends StatefulWidget {
-  const Activities({super.key});
+  final int idMap;
+  final List? plots;
+  const Activities({super.key, required this.plots, required this.idMap});
 
   @override
   State<Activities> createState() => _ActivitiesState();
 }
 
 class _ActivitiesState extends State<Activities> {
-  Widget buildCard(BuildContext context) {
-    return Card(
-      color: Colors.white,
-      child: InkWell(
-        onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return ActivityDetail();
-          }));
-        },
-        child: SizedBox(
-          height: 138.h,
-          width: 395.w,
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(15.w, 0, 15.w, 0),
-            child: Row(
+  @override
+  void initState() {
+    context.read<ActivityProvider>().loadActivities(widget.idMap);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.only(top: 10.h),
+        child: SingleChildScrollView(
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: Column(
               children: [
-                Container(
-                  width: 150.w,
-                  height: 110.h,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(18.sp),
-                      image: DecorationImage(
-                          image: AssetImage('assets/images/tree.jpg'),
-                          fit: BoxFit.cover)),
-                ),
-                SizedBox(
-                  width: 15.w,
-                ),
                 Column(
-                  crossAxisAlignment:
-                      CrossAxisAlignment.start, // จัดเนื้อหาชิดซ้าย
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text("ต้น : ",
-                        style: Theme.of(context).textTheme.bodySmall),
-                    SizedBox(
-                      width: 200.w,
-                      child: Divider(
-                        thickness: 2,
-                      ),
-                    ),
-                    Text("กิจกรรม : ",
-                        style: Theme.of(context).textTheme.bodySmall),
-                  ],
+                  children: List.generate(
+                    context.watch<ActivityProvider>().listActivities.length,
+                    (index) {
+                      ActivitiesModel activity = context
+                          .watch<ActivityProvider>()
+                          .listActivities[index];
+                      return Card(
+                        color: Colors.white,
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              return ActivityDetail(
+                                activity: activity,
+                                plots: widget.plots,
+                              );
+                            }));
+                          },
+                          child: SizedBox(
+                            height: 138.h,
+                            width: 395.w,
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(15.w, 0, 15.w, 0),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    width: 150.w,
+                                    height: 110.h,
+                                    decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.circular(18.sp),
+                                        image: DecorationImage(
+                                            onError: (exception, stackTrace) =>
+                                                () {
+                                                  print(exception);
+                                                  setState(() {
+                                                    activity.image = null
+                                                       ;
+                                                  });
+                                                },
+                                            image: FileImage(
+                                                File(activity.image!)),
+                                            fit: BoxFit.cover)),
+                                  ),
+                                  SizedBox(
+                                    width: 15.w,
+                                  ),
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment
+                                          .start, // จัดเนื้อหาชิดซ้าย
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        Text("ต้น : ${activity.tree} ",
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodySmall),
+                                        SizedBox(
+                                          width: 200.w,
+                                          child: Divider(
+                                            thickness: 2,
+                                          ),
+                                        ),
+                                        Text(
+                                          "กิจกรรม :${activity.activity} ",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodySmall,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 )
               ],
             ),
           ),
         ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return  Center(
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(0, 25.h, 0, 25.h),
-          child: Column(
-            // crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              buildCard(context),
-            ],
-          ),
-        ),
       );
-  }
+  //     ,
+  //     floatingActionButton: FloatingActionButton(
+  //       onPressed: () {
+  //         Navigator.push(context, MaterialPageRoute(builder: (context) {
+  //           return CreateActivity(
+  //             idMap: widget.idMap,
+  //             plots: widget.plots,
+  //           );
+  //         }));
+  //       },
+  //       backgroundColor: Theme.of(context).colorScheme.secondary,
+  //       child: Icon(Icons.add),
+  //     ),
+  //     floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+  //   );
+  // }
+}
 }
--- lib/pages/mainPage.dart
+++ lib/pages/mainPage.dart
@@ -2,12 +2,13 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:ui' as ui;
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:myorchard/calibrate.dart';
 import 'package:myorchard/custom_icons/customIcons.dart';
 import 'package:myorchard/pages/activities.dart';
+import 'package:myorchard/pages/chat.dart';
+import 'package:myorchard/pages/create_activity.dart';
 import 'package:myorchard/pages/edit_profile.dart';
 import 'package:myorchard/pages/profile.dart';
 
@@ -16,8 +17,13 @@ class MainPage extends StatefulWidget {
   final String name;
   final List? plots;
   final int idMap;
-  
-  const MainPage({super.key, required this.image, required this.name, this.plots, required this.idMap});
+
+  const MainPage(
+      {super.key,
+      required this.image,
+      required this.name,
+      this.plots,
+      required this.idMap});
 
   @override
   State<MainPage> createState() => _MainPageState();
@@ -28,18 +34,19 @@ class _MainPageState extends State<MainPage> {
   List<String> title = ["แผนที่", "กิจกรรม", "โปรไฟล์"];
   double scaleFactor = 0.2;
 
-  TransformationController viewTranformationController = TransformationController();
+  TransformationController viewTranformationController =
+      TransformationController();
 
   @override
   void initState() {
-    Image img = Image.file(widget.image!);
+    Image img = Image.file(widget.image);
     imageScaling(img);
     super.initState();
   }
 
   Future<void> imageScaling(Image image) async {
     final ImageStream stream = image.image.resolve(const ImageConfiguration());
-    
+
     final Completer<ui.Image> completer = Completer<ui.Image>();
     stream.addListener(ImageStreamListener((ImageInfo info, bool _) {
       completer.complete(info.image);
@@ -50,219 +57,258 @@ class _MainPageState extends State<MainPage> {
     var view = ui.PlatformDispatcher.instance.views.first;
     double deviceWidth = view.physicalSize.width;
     double imageWidth = img.width.toDouble();
-    scaleFactor = deviceWidth/ imageWidth;
-    viewTranformationController.value = Matrix4.identity()*scaleFactor;
+    scaleFactor = deviceWidth / imageWidth;
+    viewTranformationController.value = Matrix4.identity() * scaleFactor;
   }
 
   @override
   Widget build(BuildContext context) {
     List<List<Widget>> actionList = [
-      [ Padding(
-              padding: EdgeInsets.only(top: 12.h),
-              child: TextButton.icon(
-                onPressed: (){
-                  showDialog(
-                    context: context, 
-                    builder: (context) => AlertDialog(
-                      title: Text('เลือกโปรไฟล์การปรับเทียบ', style: Theme.of(context).textTheme.headlineSmall,),
+      [
+        Padding(
+          padding: EdgeInsets.only(top: 12.h),
+          child: TextButton.icon(
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                      title: Text(
+                        'เลือกโปรไฟล์การปรับเทียบ',
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
                       content: SizedBox(
                         height: 300.h,
                         child: Column(
                           children: [
                             ListTile(
-                              leading: Icon(Icons.add, size: 20.sp,),
-                              title: Text('เพิ่มการปรับเทียบ', style: Theme.of(context).textTheme.bodyMedium,),
-                              onTap: (){
+                              leading: Icon(
+                                Icons.add,
+                                size: 20.sp,
+                              ),
+                              title: Text(
+                                'เพิ่มการปรับเทียบ',
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                              onTap: () {
                                 Navigator.of(context).pop();
-                                Navigator.push(context,
-                                 MaterialPageRoute(builder: (context) => Calibrate(image: widget.image!, scale: scaleFactor,)));
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Calibrate(
+                                              image: widget.image,
+                                              scale: scaleFactor,
+                                            )));
                               },
                             )
                           ],
                         ),
-                      )
-                    ));
-                },
-                label: Text('ปรับเทียบ'),
-                iconAlignment: IconAlignment.end,
-                icon: ImageIcon(
-                  AssetImage('assets/icons/calibrate.png'),
-                  size: 30.sp, 
-                  color: Theme.of(context).colorScheme.primary,),),
-            )],
-            [
-              Padding(
-              padding: EdgeInsets.only(top: 12.h, right: 10.w),
-              child: IconButton(
-                icon: ImageIcon(
-                  AssetImage('assets/icons/upload.png'),
-                  size: 32.sp,
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
-                onPressed: () {},
-              ),
+                      )));
+            },
+            label: Text('ปรับเทียบ'),
+            iconAlignment: IconAlignment.end,
+            icon: ImageIcon(
+              AssetImage('assets/icons/calibrate.png'),
+              size: 30.sp,
+              color: Theme.of(context).colorScheme.primary,
             ),
-            Padding(
-              padding: EdgeInsets.only(top: 12.h, right: 10.w),
-              child: IconButton(
-                icon: ImageIcon(
-                  AssetImage('assets/icons/messenger.png'),
-                  size: 32.sp,
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
-                onPressed: () {},
-              ),
+          ),
+        )
+      ],
+      [
+        Padding(
+          padding: EdgeInsets.only(top: 12.h, right: 10.w),
+          child: IconButton(
+            icon: ImageIcon(
+              AssetImage('assets/icons/upload.png'),
+              size: 32.sp,
+              color: Theme.of(context).colorScheme.secondary,
             ),
-            ],
-            [Padding(
-            padding: EdgeInsets.only(top: 12.h, right: 18.w),
-            child: IconButton(
-              icon: Icon(
-                CustomIcons.edit,
-                size: 38.sp,
-                color: Theme.of(context).colorScheme.secondary,
-              ),
-              onPressed: () {
-                print("ID : ${widget.idMap}");
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return EditProfile(
-                    image: widget.image,
-                    name: widget.name,
-                    plots: widget.plots,
-                    idMap: widget.idMap,
-                  );
+            onPressed: () {},
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 12.h, right: 10.w),
+          child: IconButton(
+            icon: ImageIcon(
+              AssetImage('assets/icons/messenger.png'),
+              size: 32.sp,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            onPressed: () {
+               Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return Chat(profileId: widget.idMap,);
                 }));
-              },
+            },
+          ),
+        ),
+      ],
+      [
+        Padding(
+          padding: EdgeInsets.only(top: 12.h, right: 18.w),
+          child: IconButton(
+            icon: Icon(
+              CustomIcons.edit,
+              size: 38.sp,
+              color: Theme.of(context).colorScheme.secondary,
             ),
-          ),]
+            onPressed: () {
+              print("ID : ${widget.idMap}");
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return EditProfile(
+                  image: widget.image,
+                  name: widget.name,
+                  plots: widget.plots,
+                  idMap: widget.idMap,
+                );
+              }));
+            },
+          ),
+        ),
+      ]
     ];
     return Scaffold(
-        appBar: AppBar(
-          leading: Padding(
-            padding: EdgeInsets.only(top: 12.h, left: 10.w),
-            child: IconButton(
-              icon: Icon(
-                Icons.arrow_back_ios,
-                size: 40.h,
-                color: Theme.of(context).colorScheme.secondary,
-              ),
-              onPressed: () {
-                Navigator.pop(context);
-              },
+      appBar: AppBar(
+        leading: Padding(
+          padding: EdgeInsets.only(top: 12.h, left: 10.w),
+          child: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios,
+              size: 40.h,
+              color: Theme.of(context).colorScheme.secondary,
             ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
           ),
-          backgroundColor: Colors.white,
-          title: Text(
-            title[pageIndex],
-            style: Theme.of(context).textTheme.headlineLarge,
-          ),
-          actions: actionList[pageIndex],
-          toolbarHeight: 100.h,
         ),
-        extendBody: true,
-        bottomNavigationBar: Stack(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: 20.h),
-              child: Container(
-                height: 80.h,
-                width: double.infinity,
-                color: Colors.white,
-              ),
+        backgroundColor: Colors.white,
+        title: Text(
+          title[pageIndex],
+          style: Theme.of(context).textTheme.headlineLarge,
+        ),
+        actions: actionList[pageIndex],
+        toolbarHeight: 100.h,
+      ),
+      extendBody: true,
+      bottomNavigationBar: Stack(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 20.h),
+            child: Container(
+              height: 80.h,
+              width: double.infinity,
+              color: Colors.white,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                // ########## Map #########
-                Container(
-                  margin: pageIndex == 0 ? null : EdgeInsets.only(top: 20),
-                  height: 75.h,
-                  width: 75.w,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              // ########## Map #########
+              Container(
+                margin: pageIndex == 0 ? null : EdgeInsets.only(top: 20),
+                height: 75.h,
+                width: 75.w,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: pageIndex == 0
+                        ? Theme.of(context).colorScheme.primary
+                        : Colors.transparent),
+                child: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        pageIndex = 0;
+                      });
+                    },
+                    icon: ImageIcon(
+                      AssetImage('assets/icons/map.png'),
+                      size: 40.sp,
                       color: pageIndex == 0
-                          ? Theme.of(context).colorScheme.primary
-                          : Colors.transparent),
-                  child: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          pageIndex = 0;
-                        });
-                      },
-                      icon: ImageIcon(
-                        AssetImage('assets/icons/map.png'),
-                        size: 40.sp,
-                        color: pageIndex == 0
-                            ? Colors.white
-                            : Theme.of(context).colorScheme.secondary,
-                      )),
-                ),
-                // ########## Activity ##########
-                Container(
-                  margin: pageIndex == 1 ? null : EdgeInsets.only(top: 20),
-                  height: 75.h,
-                  width: 75.w,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
+                          ? Colors.white
+                          : Theme.of(context).colorScheme.secondary,
+                    )),
+              ),
+              // ########## Activity ##########
+              Container(
+                margin: pageIndex == 1 ? null : EdgeInsets.only(top: 20),
+                height: 75.h,
+                width: 75.w,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: pageIndex == 1
+                        ? Theme.of(context).colorScheme.primary
+                        : Colors.transparent),
+                child: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        pageIndex = 1;
+                      });
+                    },
+                    icon: ImageIcon(
+                      AssetImage('assets/icons/gardening.png'),
+                      size: 40.sp,
                       color: pageIndex == 1
-                          ? Theme.of(context).colorScheme.primary
-                          : Colors.transparent),
-                  child: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          pageIndex = 1;
-                        });
-                      },
-                      icon: ImageIcon(
-                        AssetImage('assets/icons/gardening.png'),
-                        size: 40.sp,
-                        color: pageIndex == 1
-                            ? Colors.white
-                            : Theme.of(context).colorScheme.secondary,
-                      )),
-                ),
-                // ########## Profile ##########
-                Container(
-                  margin: pageIndex == 2 ? null : EdgeInsets.only(top: 20),
-                  height: 75.h,
-                  width: 75.w,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
+                          ? Colors.white
+                          : Theme.of(context).colorScheme.secondary,
+                    )),
+              ),
+              // ########## Profile ##########
+              Container(
+                margin: pageIndex == 2 ? null : EdgeInsets.only(top: 20),
+                height: 75.h,
+                width: 75.w,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: pageIndex == 2
+                        ? Theme.of(context).colorScheme.primary
+                        : Colors.transparent),
+                child: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        pageIndex = 2;
+                      });
+                    },
+                    icon: Icon(
+                      Icons.person,
+                      size: 40.sp,
                       color: pageIndex == 2
-                          ? Theme.of(context).colorScheme.primary
-                          : Colors.transparent),
-                  child: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          pageIndex = 2;
-                        });
-                      },
-                      icon: Icon(
-                        Icons.person,
-                        size: 40.sp,
-                        color: pageIndex == 2
-                            ? Colors.white
-                            : Theme.of(context).colorScheme.secondary,
-                      )),
-                ),
-              ],
-            )
-          ],
-        ),
-        body: [
-          Center(
-              child: InteractiveViewer(
-                  minScale: 0.2,
-                  transformationController: viewTranformationController,
-                  constrained: false,
-                  child: Image(image: FileImage(widget.image)))),
-          Activities(),
-          Profile(
-            image: widget.image,
-            name: widget.name,
-            plots: widget.plots, 
-            idMap: widget.idMap,
+                          ? Colors.white
+                          : Theme.of(context).colorScheme.secondary,
+                    )),
+              ),
+            ],
           )
-        ][pageIndex]);
+        ],
+      ),
+      body: [
+        Center(
+            child: InteractiveViewer(
+                minScale: 0.2,
+                transformationController: viewTranformationController,
+                constrained: false,
+                child: Image(image: FileImage(widget.image)))),
+        Activities(plots: widget.plots, idMap: widget.idMap),
+        Profile(
+          image: widget.image,
+          name: widget.name,
+          plots: widget.plots,
+          idMap: widget.idMap,
+        )
+      ][pageIndex],
+      floatingActionButton: [
+        null,
+        FloatingActionButton(
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return CreateActivity(
+                  idMap: widget.idMap,
+                  plots: widget.plots,
+                );
+              }));
+            },
+            backgroundColor: Theme.of(context).colorScheme.secondary,
+            child: Icon(Icons.add)),
+        null
+      ][pageIndex],
+    );
   }
 }
